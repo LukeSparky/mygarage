fe6640102a78a87f3593d395d9ecd8909d993c98
Lucas Passarella <lucaspassarella@hotmail.com>
2022-04-20 13:01:56 -0400

    -added sign out button with limited functionality -installed turbo

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
   before_action :set_car, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [ :index, :new, :edit, :destroy ]
 
   # GET /cars or /cars.json
   def index
